65215f1e639f151217553f32337c829d177cc84d
Willem Gibson <willemjackgibson@gmail.com>
2024-11-12 09:35:43 +1100

    feat: adding title to portfolio page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -57,88 +57,91 @@ export default function WorkPage() {
       <BgScene className="fixed top-0 left-0 h-screen w-screen z-10 pointer-events-none" />
 
       {/* Card container with responsive 2-column grid */}
-      <div className="fixed w-full max-w-screen-xl h-[800px] overflow-y-scroll no-scrollbar rounded-lg shadow-lg border-4 px-12 py-16 grid grid-cols-1 sm:grid-cols-2 gap-8 z-20">
-        {/* Card 1 */}
-        <div
-          className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
-          onClick={() => openModal(0)}
-        >
-          <div className="relative w-full h-full">
-            <img
-              src="assets/projects/proj1/1.avif"
-              alt="Project 1"
-              className="w-full h-full object-cover"
-            />
+      <div className="fixed w-full max-w-screen-xl h-[800px] bg-black overflow-y-scroll no-scrollbar rounded-lg shadow-lg border-4 py-12 px-8 z-20">
+        <h1 className="text-3xl sm:text-4xl font-bold mb-4 text-center md:text-center">My Work</h1>
+        <div className="p-8 grid grid-cols-1 sm:grid-cols-2 gap-8">
+          {/* Card 1 */}
+          <div
+            className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
+            onClick={() => openModal(0)}
+          >
+            <div className="relative w-full h-full">
+              <img
+                src="assets/projects/proj1/1.avif"
+                alt="Project 1"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Card 2 */}
-        <div
-          className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
-          onClick={() => openModal(1)}
-        >
-          <div className="relative w-full h-full">
-            <img
-              src="assets/projects/proj2/1.avif"
-              alt="Project 2"
-              className="w-full h-full object-cover"
-            />
+          {/* Card 2 */}
+          <div
+            className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
+            onClick={() => openModal(1)}
+          >
+            <div className="relative w-full h-full">
+              <img
+                src="assets/projects/proj2/1.avif"
+                alt="Project 2"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Card 3 */}
-        <div
-          className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
-          onClick={() => openModal(2)}
-        >
-          <div className="relative w-full h-full">
-            <img
-              src="assets/projects/proj3/1.avif"
-              alt="Project 3"
-              className="w-full h-full object-cover"
-            />
+          {/* Card 3 */}
+          <div
+            className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
+            onClick={() => openModal(2)}
+          >
+            <div className="relative w-full h-full">
+              <img
+                src="assets/projects/proj3/1.avif"
+                alt="Project 3"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Card 4 */}
-        <div
-          className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
-          onClick={() => openModal(3)}
-        >
-          <div className="relative w-full h-full">
-            <img
-              src="assets/projects/proj4/1.avif"
-              alt="Project 4"
-              className="w-full h-full object-cover"
-            />
+          {/* Card 4 */}
+          <div
+            className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
+            onClick={() => openModal(3)}
+          >
+            <div className="relative w-full h-full">
+              <img
+                src="assets/projects/proj4/1.avif"
+                alt="Project 4"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Card 5 */}
-        <div
-          className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
-          onClick={() => openModal(4)}
-        >
-          <div className="relative w-full h-full">
-            <img
-              src="assets/projects/proj5/1.avif"
-              alt="Project 5"
-              className="w-full h-full object-cover"
-            />
+          {/* Card 5 */}
+          <div
+            className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
+            onClick={() => openModal(4)}
+          >
+            <div className="relative w-full h-full">
+              <img
+                src="assets/projects/proj5/1.avif"
+                alt="Project 5"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
-        </div>
 
-        {/* Card 6 */}
-        <div
-          className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
-          onClick={() => openModal(5)}
-        >
-          <div className="relative w-full h-full">
-            <img
-              src="assets/projects/proj6/1.avif"
-              alt="Project 6"
-              className="w-full h-full object-cover"
-            />
+          {/* Card 6 */}
+          <div
+            className="w-full h-[400px] bg-black border-2 border-white rounded-lg overflow-hidden shadow-lg transition-transform duration-300 hover:scale-105 relative z-20"
+            onClick={() => openModal(5)}
+          >
+            <div className="relative w-full h-full">
+              <img
+                src="assets/projects/proj6/1.avif"
+                alt="Project 6"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
         </div>
       </div>
